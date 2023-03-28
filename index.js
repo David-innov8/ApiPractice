ab9334c1609cc12c19d3f16d0b71727fc446a504
David-innov8 <davidowolabi899@gmail.com>
2023-03-28 11:42:48 +0100

    test

---
+++ index.js
@@ -0,0 +1,37 @@
+const api_url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+
+const result = document.getElementById("result")
+const sound = document.getElementById("sound")
+
+const btn = document.getElementById("search-btn")
+
+
+btn.addEventListener("click", ()=>{
+    let inpWord  =  document.getElementById("input-word").value;
+
+    fetch(`${api_url}${inpWord}`)
+    .then(reponse =>reponse.json () )
+    .then((data) =>{
+        console.log(data)
+        result.innerHTML = `
+        <div class="word">
+                
+        <h3>${inpWord}</h3>
+        <button>
+
+        </button>
+    </div>
+    <div class="details">
+        <p>${data[0].meanings[0].partOfSpeech}</p>
+        <p>${data[0].phonetic}</p>
+    </div>
+    <p class="word-meaning">
+    ${data[0].meanings[0].definitions[0].definition}
+    </p>
+    <p class="word-example">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, aliquid!
+    </p>
+</div>
+        `
+    } )
+}) 
